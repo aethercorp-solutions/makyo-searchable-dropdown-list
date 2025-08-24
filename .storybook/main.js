f82adcef4353849c8d7cfa349bdcd59be9d9d40c
pauld <pauld@xelure.com>
2025-08-24 23:26:37 +0800

    fix local storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,15 @@
 
 
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const path = require('path');
 const config = {
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    };
+    return config;
+  },
   "stories": [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
