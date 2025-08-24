7db66910c45d5856db5ef027073b638d2f4d1ea8
pauld <pauld@xelure.com>
2025-08-24 13:37:27 +0800

    feature(multi-select-dropdown): add searchable, accessible multi-select dropdown with portal and outlined styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,3 @@
 import './components/MultiSelectDropdown.css';
 export { default as MultiSelectDropdown } from './components/MultiSelectDropdown';
-
+export * from './components/MultiSelectDropdown';
